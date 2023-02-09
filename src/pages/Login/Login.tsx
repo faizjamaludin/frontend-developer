7c4605be2bb2f5f749fbfc7671248ec53f8e0314
unknown <faiz.jamaludin02@gmail.com>
2023-02-09 23:27:56 +0800

    handling onchange

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../form.css";
 import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
 
+type AuthUser = {
+  mobile: string;
+  password: string;
+};
+
 function Login() {
+  const [auth, setAuth] = useState<AuthUser>({} as AuthUser);
+
   return (
     <div className="login-box">
       <p>Login</p>
       <form>
         <div className="user-box">
-          <input required name="" type="text" />
+          <input required name="mobile" type="text" />
           <label>Mobile</label>
         </div>
         <div className="user-box">
-          <input required name="" type="password" />
+          <input required name="password" type="password" />
           <label>Password</label>
         </div>
         <Button />
